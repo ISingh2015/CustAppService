380dec7d35ac5789112f67af5b569881cb4de7af
Inderjit <inderjitsanhotra@gmail.com>
2019-02-12 13:07:58 +0530

    Updated: Database PORT Property

--- src/main/java/com/cust/common/session/DynamicDataSource.java
+++ src/main/java/com/cust/common/session/DynamicDataSource.java
@@ -3,18 +3,22 @@ package com.cust.common.session;
 import com.cust.common.DBInfo;
 import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 
 public class DynamicDataSource {
 
-	@Autowired
-	private DBPropertyContainer propertyContainer;
+    @Autowired
+    private DBPropertyContainer propertyContainer;
 
-	public BasicDataSource getDataSource(DBInfo dbInfo) {
-		BasicDataSource ds = new BasicDataSource();
-                ds.setDriverClassName(propertyContainer.getDbDriverClassName());
-		ds.setUrl("jdbc:sqlserver://" + propertyContainer.getDbServerName() + ":1433;databaseName=" + dbInfo.getDbName());
-		ds.setUsername(propertyContainer.getDbUserName());
-		ds.setPassword(propertyContainer.getDbPassword());
-		return ds;
-	}
+    @Value("${db.serverPort}")
+    private String dbPort;
+
+    public BasicDataSource getDataSource(DBInfo dbInfo) {
+        BasicDataSource ds = new BasicDataSource();
+        ds.setDriverClassName(propertyContainer.getDbDriverClassName());
+        ds.setUrl("jdbc:sqlserver://" + propertyContainer.getDbServerName() + ":" + dbPort + ";databaseName=" + dbInfo.getDbName());
+        ds.setUsername(propertyContainer.getDbUserName());
+        ds.setPassword(propertyContainer.getDbPassword());
+        return ds;
+    }
 }
